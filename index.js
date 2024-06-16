cd80dd61d8fb0ac52091b4ec0fb59b36adc8cabf
Farhad2590 <farhadhossen2590@gmail.com>
2024-06-16 16:50:55 +0600

    user api setted and all user api created

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ async function run() {
 
     const mealsCollection = client.db("eatEassyDb").collection("meals");
     const reviewCollection = client.db("eatEassyDb").collection("reviews");
+    const userCollection = client.db("eatEassyDb").collection("users");
 
     app.get('/meals', async (req, res) => {
       const result = await mealsCollection.find().toArray();
@@ -68,12 +69,12 @@ async function run() {
       // console.log(size, page)
 
       let query = {
-        title:{$regex: search, $options: 'i'}
+        title: { $regex: search, $options: 'i' }
       }
       if (filter) query.category = filter
       let options = {}
       if (sort) options = { sort: { price: sort === 'asc' ? 1 : -1 } }
-      const result = await mealsCollection.find(query,options).skip(page * size).limit(size).toArray()
+      const result = await mealsCollection.find(query, options).skip(page * size).limit(size).toArray()
 
       res.send(result)
     })
@@ -83,7 +84,7 @@ async function run() {
       const filter = req.query.filter
       const search = req.query.search
       let query = {
-        title:{$regex: search, $options: 'i'}
+        title: { $regex: search, $options: 'i' }
       }
       if (filter) query.category = filter
       const count = await mealsCollection.countDocuments(query)
@@ -91,6 +92,51 @@ async function run() {
       res.send({ count })
     })
 
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+
+      // if (email !== req.decoded.email) {
+      //   return res.status(403).send({ message: 'forbidden access' })
+      // }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // insert email if user doesnt exists: 
+      // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     )
