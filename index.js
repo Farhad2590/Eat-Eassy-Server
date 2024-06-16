02db6a7913a6d329fa0bcd0215db5411b93b6823
Farhad2590 <farhadhossen2590@gmail.com>
2024-06-16 13:56:10 +0600

    In all meal page all-meal api added and search and filter functiona;ity added

--- index.js
+++ index.js
@@ -31,19 +31,65 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // auth related api
-    
+
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
 
-    
-    const mealsCollection = client.db("eatEassyDb").collection("meals");
 
-    app.get('/meals', async(req, res) =>{
+    const mealsCollection = client.db("eatEassyDb").collection("meals");
+    const reviewCollection = client.db("eatEassyDb").collection("reviews");
+
+    app.get('/meals', async (req, res) => {
       const result = await mealsCollection.find().toArray();
       res.send(result);
-  })
+    })
 
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/reviews', async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await reviewCollection.insertOne(newProduct)
+      res.send(result)
+    })
+
+
+    // Get all jobs data from db for pagination
+    app.get('/all-meals', async (req, res) => {
+      const size = parseInt(req.query.size)
+      const page = parseInt(req.query.page) - 1
+      const filter = req.query.filter
+      const sort = req.query.sort
+      const search = req.query.search
+      // console.log(size, page)
+
+      let query = {
+        title:{$regex: search, $options: 'i'}
+      }
+      if (filter) query.category = filter
+      let options = {}
+      if (sort) options = { sort: { price: sort === 'asc' ? 1 : -1 } }
+      const result = await mealsCollection.find(query,options).skip(page * size).limit(size).toArray()
+
+      res.send(result)
+    })
+
+    // Get all jobs data count from db
+    app.get('/meals-count', async (req, res) => {
+      const filter = req.query.filter
+      const search = req.query.search
+      let query = {
+        title:{$regex: search, $options: 'i'}
+      }
+      if (filter) query.category = filter
+      const count = await mealsCollection.countDocuments(query)
+
+      res.send({ count })
+    })
 
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
